fd6898ce944db92e400e7aafeba4195f148c5357
Dean <dean@airdb.com>
2020-07-17 14:41:09 +0800

    typo and gofmt
    Signed-off-by: deancn <dean@airdb.com>

--- bfe_modules/mod_markdown/mod_markdown.go
+++ bfe_modules/mod_markdown/mod_markdown.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// package mod mark_down, deal with the markdown`s repsonse
+// package mod mark_down, deal with the markdown`s response
 
 package mod_markdown
 
