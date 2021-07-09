1f77a4e3a970acd15d51be21294cb1f17daa4c62
Sijie Yang <yangsijie@baidu.com>
2021-07-09 17:47:51 +0800

    Update bfe_module_test.go

--- bfe_module/bfe_module_test.go
+++ bfe_module/bfe_module_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The BFE Authors.
+// Copyright (c) 2021 The BFE Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
